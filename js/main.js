e9f86276297ba7a7fd77163f13b7e9a0e1d36525
janetccc <chanj764@newschool.edu>
2015-10-21 12:03:44 -0400

    made edits to header styling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 $('.city_graph').hover(function() {
 	$(this).find('h4').css('color', '#333399');
-	$(this).find('h4').css('background', '#22ddcc');
+	$(this).find('.city_name_bar').css('background', '#333399');
+	// $(this).find('h4').css('background', '#22ddcc');
 	$(this).find('a').css('background', 'none');
+}, function() {
+	$(this).find('h4').css('color', '#22ddcc');
+	$(this).find('.city_name_bar').css('background', '#22ddcc');
+});
+
+$('.bottom_links a').hover(function() {
+	$(this).css('background-color', '#000');
+	console.log('yes')
+	// $(this).find('h4').css('background', '#22ddcc');
+	// $(this).find('h4').css('background', '#22ddcc');
+	// $(this).find('a').css('background', 'none');
 }, function() {
 	$(this).find('h4').css('color', '#22ddcc');
 	$(this).find('h4').css('background', 'none');
